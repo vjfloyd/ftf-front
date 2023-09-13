9caa0256e45a8d0f92e2856bfb399f257a8bf220
victorrojasb <victor.rojas@nautilus.team>
2023-09-13 01:28:47 -0500

    feat:init project

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// import React from "react";
+import "./App.css";
+import Navigation from "./components/navigation/navigation.component";
+import Menu from "./components/menu/menu.component";
+import Commits from "./components/commits/commit.component";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Dashboard from "./components/dashboard/dashboard.component";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Navigation />
+        <div className="container-fluid">
+          <div className="row">
+            <Menu />
+            <main className="col-md-9 ms-sm-auto col-lg-10 px-md-4">
+              <div className="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+                <h1 className="h2">Commits History</h1>
+              </div>
+
+              <Routes>
+                <Route path={"/"} element={<Dashboard />} />
+                <Route path={"/commits"} element={<Commits />} />
+              </Routes>
+            </main>
+          </div>
+        </div>
+      </div>
+    </Router>
   );
 }
 
